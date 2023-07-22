27b767b7d25c0660ac52a644e7fb395f09c083e8
bimtaiht <bimtaiht@gmail.com>
2023-07-22 23:50:47 +0700

    update

---
+++ Command/RevitCommand/0.Command/Command/Command.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Model.Form;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Model.Entity;
+
+namespace Model.RevitCommand
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : RevitCommand
+    {
+        public override void Execute()
+        {
+            
+        }
+
+        public override RibbonConfig? RibbonConfig => new()
+        {
+            Tab = "<tab name>",
+            Panel = "<panel name>",
+            Name = "<command name>",
+            IconPath = "Resource/Icon/addin.ico"
+        };
+    }
+}
